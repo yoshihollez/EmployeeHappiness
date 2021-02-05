71874fb4238f9e9539a36413c2ff2eb11358bb92
yoshihollez <36663359+yoshihollez@users.noreply.github.com>
2021-02-05 13:04:03 +0100

    added buttons for happiness ratings

--- src/routes/Rating.js
+++ src/routes/Rating.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Rating.css";
+import { Button, Icon } from "react-materialize";
 
 export default class Rating extends React.Component {
   constructor(props) {
@@ -10,6 +12,35 @@ export default class Rating extends React.Component {
     return (
       <div>
         <p>rating</p>
+        <div className="buttonContainer">
+          <div>
+            <Button
+              large
+              className="red"
+              floating
+              icon={<Icon large>sentiment_very_dissatisfied</Icon>}
+              node="button"
+            />
+          </div>
+          <div>
+            <Button
+              large
+              className="red"
+              floating
+              icon={<Icon large>sentiment_neutral</Icon>}
+              node="button"
+            />
+          </div>
+          <div>
+            <Button
+              large
+              className="red"
+              floating
+              icon={<Icon large>sentiment_very_satisfied</Icon>}
+              node="button"
+            />
+          </div>
+        </div>
       </div>
     );
   }
