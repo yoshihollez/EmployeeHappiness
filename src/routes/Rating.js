51b705fbce53b47b96ceeb3af543f712b71233fc
yoshihollez <36663359+yoshihollez@users.noreply.github.com>
2021-02-05 12:41:04 +0100

    created Rating component

---
+++ src/routes/Rating.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Rating extends React.Component {
+  constructor(props) {
+    super(props);
+    M.AutoInit();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <p>rating</p>
+      </div>
+    );
+  }
+}
