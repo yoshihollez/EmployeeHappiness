89f574780783b0018ce51e78c78a0fe299e763d4
yoshihollez <36663359+yoshihollez@users.noreply.github.com>
2021-02-05 17:47:32 +0100

    added graph with test values

--- src/routes/Statistics.js
+++ src/routes/Statistics.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Statistics.css";
+import { Chart } from "react-charts";
 
 export default class Statistics extends React.Component {
   constructor(props) {
@@ -8,8 +10,49 @@ export default class Statistics extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="Statistics">
         <p>statistics</p>
+        <div className="graph">
+          <Chart
+            data={[
+              {
+                label: "Series 1",
+                data: [
+                  [0, 1],
+                  [1, 2],
+                  [2, 4],
+                  [3, 2],
+                  [4, 7],
+                ],
+              },
+              {
+                label: "Series 2",
+                data: [
+                  [0, 3],
+                  [1, 1],
+                  [2, 5],
+                  [3, 6],
+                  [4, 4],
+                ],
+              },
+              {
+                label: "Series 3",
+                data: [
+                  [0, 5],
+                  [1, 1],
+                  [2, 1],
+                  [3, 1],
+                  [4, 4],
+                ],
+              },
+            ]}
+            axes={[
+              { primary: true, type: "linear", position: "bottom" },
+              { type: "linear", position: "left" },
+            ]}
+            tooltip
+          />
+        </div>
       </div>
     );
   }
