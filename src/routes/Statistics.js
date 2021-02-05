a60d86cf9a85cc507a78eed95afad0375c467ea1
yoshihollez <36663359+yoshihollez@users.noreply.github.com>
2021-02-05 12:41:16 +0100

    created Statistics component

---
+++ src/routes/Statistics.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Statistics extends React.Component {
+  constructor(props) {
+    super(props);
+    M.AutoInit();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <p>statistics</p>
+      </div>
+    );
+  }
+}
