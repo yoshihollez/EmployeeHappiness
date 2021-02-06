93ad8d6c04d36321ce5d09ee93f799af997c978c
yoshihollez <36663359+yoshihollez@users.noreply.github.com>
2021-02-06 22:41:09 +0100

    added pie chart for happiness values

--- src/routes/Statistics.js
+++ src/routes/Statistics.js
@@ -1,24 +1,42 @@
 import React from "react";
 import "./Statistics.css";
-import { Chart } from "react-charts";
 import { Button } from "react-materialize";
+import { PieChart } from "react-minimal-pie-chart";
 
 export default class Statistics extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      responseMessage: "",
+      averageEmployeeHappiness: 0,
+      totalEmployeeHappiness: {
+        totalEmployeesDissatisfied: 1,
+        totalEmployeesNeutral: 1,
+        totalEmployeesSatisfied: 1,
+      },
+    };
   }
+
+  componentDidMount = () => {
+    this.getEmployeeHappinessRatings("day");
+  };
 
   getEmployeeHappinessRatings = async (period) => {
     fetch(`http://localhost:4000/getEmployeeHappinessAverage/${period}`)
       .then((response) => response.json())
-      .then((data) => this.setState({ responseMessage: data.responseMessage }));
+      .then((data) =>
+        this.setState({
+          responseMessage: data.responseMessage,
+          averageEmployeeHappiness: data.averageEmployeeHappiness,
+          totalEmployeeHappiness: JSON.parse(data.totalEmployeeHappiness),
+        })
+      );
   };
 
   render() {
     return (
       <div className="Statistics">
-        <p>statistics</p>
+        <p>{this.state.responseMessage}</p>
         <Button
           onClick={() => {
             this.getEmployeeHappinessRatings("day");
@@ -40,25 +58,33 @@ export default class Statistics extends React.Component {
         >
           month
         </Button>
+
         <div className="graph">
-          <Chart
+          <PieChart
+            label={({ dataEntry }) =>
+              `${dataEntry.title}: \n ${Math.round(dataEntry.percentage)} %`
+            }
+            labelStyle={{ fontSize: "25%", fontFamily: "sans-serif" }}
+            animate
             data={[
               {
-                label: "Series 1",
-                data: [
-                  [0, 1],
-                  [1, 2],
-                  [2, 4],
-                  [3, 2],
-                  [4, 7],
-                ],
+                title: "Dissatisfied",
+                value: this.state.totalEmployeeHappiness
+                  .totalEmployeesDissatisfied,
+                color: "#E38627",
+              },
+              {
+                title: "Neutral",
+                value: this.state.totalEmployeeHappiness.totalEmployeesNeutral,
+                color: "#C13C37",
+              },
+              {
+                title: "Satisfied",
+                value: this.state.totalEmployeeHappiness
+                  .totalEmployeesSatisfied,
+                color: "#6A2135",
               },
             ]}
-            axes={[
-              { primary: true, type: "linear", position: "bottom" },
-              { type: "linear", position: "left" },
-            ]}
-            tooltip
           />
         </div>
       </div>
