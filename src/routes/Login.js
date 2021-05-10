2a15fdd1db045aa646760de85d28c616ee0e7015
yoshihollez <36663359+yoshihollez@users.noreply.github.com>
2021-05-10 14:54:14 +0200

    added error checks and messages for login and register

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -4,8 +4,9 @@ import { Button } from "react-materialize";
 import { useMutation, useLazyQuery } from "@apollo/client";
 import gql from "graphql-tag";
 import "./Login.css";
-import { useHistory } from "react-router-dom";
-
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+// API call to graphql backend to login, also sets a cookie so you can stay logged in for 30min.
 const LOGIN = gql`
   query login($password: String!, $username: String!) {
     login(password: $password, username: $username) {
@@ -20,6 +21,8 @@ const LOGIN = gql`
     }
   }
 `;
+
+// API call to graphql backend to create a new user.
 const CREATE_USER = gql`
   mutation CreateUser($password: String!, $username: String!) {
     createUser(password: $password, username: $username) {
@@ -36,9 +39,15 @@ const CREATE_USER = gql`
 `;
 
 export default function Login(props) {
+  const [usernameError, setUsernameError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [usernameErrorMessage, setUsernameErrorMessage] = useState("");
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+  const [checkForError, setCheckForError] = useState(true);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [login, { loading, error, data }] = useLazyQuery(LOGIN, {
+  const [createUserData, setCreateUserData] = useState();
+  const [userLogin, { loading, error, data }] = useLazyQuery(LOGIN, {
     variables: {
       password: password,
       username: username,
@@ -50,47 +59,91 @@ export default function Login(props) {
       username: username,
     },
   });
-  const history = useHistory();
 
-  if (loading) {
-    // gives warning that you shouldn't update during a transition.
-    // redirects to default page after succesfull login.
-    // history.push("/");
+  if (
+    checkForError &&
+    (data?.login.errors || createUserData?.data.createUser.errors)
+  ) {
+    let errorField;
+    let errorMessage = data?.login.errors
+      ? data?.login.errors[0].message
+      : createUserData?.data.createUser.errors[0].message;
+    if (data) {
+      errorField =
+        data?.login.errors[0].field == "username" ? "username" : "password";
+    } else {
+      errorField =
+        createUserData?.data.createUser.errors[0].field == "username"
+          ? "username"
+          : "password";
+    }
+    console.log(errorField);
+    if (errorField == "username") {
+      setPasswordError(false);
+      setUsernameError(true);
+      setUsernameErrorMessage(errorMessage);
+      setPasswordErrorMessage("");
+    } else {
+      setUsernameError(false);
+      setPasswordError(true);
+      setPasswordErrorMessage(errorMessage);
+      setUsernameErrorMessage("");
+    }
+    setCheckForError(false);
   }
-  return (
-    <div className="container">
-      <div className="inputBox">
-        <p>Username</p>
-        <input
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          id="username"
-        />
-        <p>password</p>
-        <input
-          label="Password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          id="password"
-        />
+  if (data?.login.user || createUserData?.data.createUser.user) {
+    let name = "";
+    console.log(data);
+    if (createUserData) name = createUserData.data.createUser.user.username;
+    if (data?.login) name = data.login.user.username;
+    return (
+      <div>
+        <h3>You have succesfully logged in as {name}.</h3>
       </div>
-      <div className="buttons">
-        <Button
-          onClick={() => {
-            login();
-          }}
-        >
-          Login
-        </Button>
-        <Button
-          onClick={() => {
-            createUser();
-          }}
-        >
-          Register
-        </Button>
+    );
+  } else {
+    return (
+      <div className="container">
+        <div className="inputBox">
+          <p>Username</p>
+          <TextField
+            error={usernameError}
+            id="username"
+            label="username"
+            defaultValue=""
+            helperText={usernameErrorMessage}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <p>password</p>
+          <TextField
+            error={passwordError}
+            id="password"
+            label="password"
+            type="password"
+            defaultValue=""
+            helperText={passwordErrorMessage}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="buttons">
+          <Button
+            onClick={() => {
+              userLogin();
+              setCheckForError(true);
+            }}
+          >
+            Login
+          </Button>
+          <Button
+            onClick={async () => {
+              setCreateUserData(await createUser());
+              setCheckForError(true);
+            }}
+          >
+            Register
+          </Button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
